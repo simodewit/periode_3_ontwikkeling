031f61b78294cd24acba5600708c59030b915d16
kingkaiSenpai <118214343+kingkaiSenpai@users.noreply.github.com>
2023-03-17 16:15:35 +0100

    verder met ui

---
+++ periode 3 ontwikkeling/Assets/scripts/LoadScrene.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using Unity.VisualScripting;
+using UnityEngine.SceneManagement;
+
+public class LoadScrene : MonoBehaviour
+{
+    public Image loadImage;
+    public float loadTransition;
+    public Color loadColor;
+    public bool loadTrigger;
+    public bool endLoad;
+    public float loadScreenTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        loadImage.color = loadColor;
+
+        if (loadImage.color.a < 1 && loadTrigger)
+        {
+            loadColor.a += loadTransition;
+        }
+        else
+        {
+            if(endLoad == true)
+            {
+                loadColor.a -= loadTransition;
+            }
+        }
+    }
+    public void NextScene()
+    {
+        StartCoroutine(StartLoading());
+    }
+
+    public IEnumerator StartLoading()
+    {
+        endLoad = false;
+        loadTrigger = true;
+        yield return new WaitForSeconds(loadScreenTime);
+        loadTrigger = false;
+        SceneManager.LoadScene("GameplayScene");
+        yield return new WaitForSeconds(loadScreenTime);
+        endLoad = true;
+    }
+}
